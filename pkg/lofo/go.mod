207c8e4fa305eaccc4f4addb6717c6327658546e
dudung <dudung@gmail.com>
2022-11-12 15:47:10 +0700

    update, fin lofo

---
+++ pkg/lofo/go.mod
@@ -0,0 +1,3 @@
+module github.com/dudung/sego/pkg/lofo
+
+go 1.19
---
+++ pkg/lofo/lofo.go
@@ -0,0 +1,13 @@
+/*
+  lofo.go
+  A module on a local folder to be called by ping.go
+  
+  Sprisoma Viridi | https://github.com/dudung
+  
+  20221112 Create it.
+*/
+package lofo
+
+func Ping() string {
+  return "Pong from lofo"
+}
