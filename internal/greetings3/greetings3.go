eb5b30446d33d5763581cc2402bece544e1c2ee1
dudung <dudung@gmail.com>
2022-11-12 17:42:42 +0700

    fin hi3 and greetings3, init function

---
+++ internal/greetings3/greetings3.go
@@ -0,0 +1,46 @@
+/*
+  greetings3.go
+  A module with error handler to be called by hi3.go
+  
+  Sparisoma Viridi | https://github.com/dudung
+  
+  20221112 Copy [1] and modifyt it.
+  
+  refs
+  1. url https://go.dev/doc/tutorial/random-greeting [20221112].
+*/
+
+package greetings3
+
+import (
+  "errors"
+  "fmt"
+  "math/rand"
+  "time"
+)
+
+func Hello(name string) (string, error) {
+  if name == "" {
+    return "", errors.New("Empty name")
+  }
+  
+  msg := fmt.Sprintf(randomFormat(), name)
+  return msg, nil
+}
+
+
+func init() {
+  rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+  formats := [] string {
+    "Hi, %v. Welcome to Go world!",
+    "Great to see you, %v!",
+    "Hail, %v! Well met!",
+    "Schoen Sie, kennenzulernen, %v!",
+    "Apa kabar, %v! Selamat datang di dunia Go.",
+  }
+  
+  return formats[rand.Intn(len(formats))]
+}
